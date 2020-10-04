da7815d52a1471290a5be197910380218f0035ef
Archie <a.edwaar@gmail.com>
2020-10-04 12:10:19 +0100

    Updates

---
+++ precache-manifest.27f2a5e84bfe6a2061843d048ffcf25a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4519c99050d44b00ae198aa951b05af",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "c701d99b599e4b52be95",
+    "url": "/personal-site/static/css/main.8e7ec60c.chunk.css"
+  },
+  {
+    "revision": "7dbedd7e8a9c7491a220",
+    "url": "/personal-site/static/js/2.414da3c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-site/static/js/2.414da3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c701d99b599e4b52be95",
+    "url": "/personal-site/static/js/main.4d532f73.chunk.js"
+  },
+  {
+    "revision": "68a0f636c09dd34dd548",
+    "url": "/personal-site/static/js/runtime-main.399b384f.js"
+  },
+  {
+    "revision": "79de436751ee47d230eb1af87439a363",
+    "url": "/personal-site/static/media/profile.79de4367.png"
+  }
+]);
