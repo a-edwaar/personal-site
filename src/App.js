30215a09cc7662514ce0407353a3aeb382b076ea
Archie <a.edwaar@gmail.com>
2020-10-04 12:01:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./profile.png";
+import "./App.css";
+import Socials from "./socials";
 
 function App() {
   return (
     <div className="App">
+      <Socials />
+      <hr />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hey, I'm Archie</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Developer, Student and Blogger.
+          <br></br>Currently working on a SaaS product for my final year uni
+          project.
+          <br></br> Previously @IBM and developing{" "}
+          <a
+            className="App-link"
+            href="https://apps.apple.com/gb/app/co-shop/id1513591835"
+          >
+            Co-shop.
+          </a>
+          <br />
+          📍 Birmingham, UK
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/socials.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./socials.css";
+
+function Socials() {
+  return (
+    <div className="Socials">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://medium.com/@a.edwaar">📝 Blog</a>
+          </li>
+          <li>
+            <a href="https://github.com/a-edwaar">🔧 Github</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/aedwds">🐦 Twitter</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/a-edwaar/">🙋🏻‍♂️ LinkedIn</a>
+          </li>
+          <li>
+            <a href="mailto:a.edwaar@gmail.com">💌 Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Socials;
